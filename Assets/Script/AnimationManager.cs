9cbd926ac1647684e9a78543eb878005bf94817d
No0801 <matthis.pillonel@wanadoo.fr>
2023-11-06 10:45:57 +0100

    initial push

---
+++ Assets/Script/AnimationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public Animator animator;
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    public void Move(float vitesse)
+    {
+        animator.SetFloat("vitesse", vitesse);
+    }
+
+    public void SetAxis(float X, float Z)
+    {
+        animator.SetFloat("X", X);
+        animator.SetFloat("Z", Z);
+    }
+}
---
+++ Assets/Script/EnnemiMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnnemiMovement : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rigi;
+    public float vitesse;
+    public AnimationManager animationManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationManager.Move(rigi.velocity.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = (target.position - transform.position).normalized;
+        dir.y = 0;
+        rigi.velocity = dir.normalized * vitesse;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
